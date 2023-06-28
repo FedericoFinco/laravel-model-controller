06caa2aeac0ac0d24b79265a05a2ea710ace9cbd
FedericoFinco <doppiaeffe94@gmail.com>
2023-06-28 18:52:27 +0200

    dev(main)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container my-3">
-    <h1>Welcome Page</h1>
-    <div class="row g-4">
-        <div class="col">
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, delectus ad esse illum omnis earum eligendi sint a minus quasi, inventore nulla autem. Maxime voluptatem eligendi veniam voluptates. Soluta, sunt!</p>
+<div class="container-fluid">
+    <div class="row flex-wrap">
+        @foreach ($movies as $movie)
+            <div class="col-4 p-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h2 class="card-title">{{ $movie->title }}</h2>
+                        <h3 class="card-text">{{ $movie->original_title }}</h3>
+                        <h4>{{ $movie->date }}</h4>
+                        <h5>Language: {{ $movie->nationality }}</h5>
+                        <h5>Rating: {{ $movie->vote }}</h5>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-
-</div>
+        @endforeach
 @endsection
